caf11635b3f1ec8cb4a5fb35cf640fbde05317d5
Qing Li <lqingpku@gmail.com>
2019-03-04 15:18:22 -0700

    Add support for vertical cross sections

--- mpasanalysis.py
+++ mpasanalysis.py
@@ -4,7 +4,86 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from netCDF4 import Dataset
+from scipy import spatial
 from mpl_toolkits.basemap import Basemap
+
+#--------------------------------
+# MPASOVolume
+#--------------------------------
+
+class MPASOVolume(object):
+
+    """MPASOVolume object"""
+
+    def __init__(self, data, lon, lat, depth, cellarea, name, units):
+        """Iniitalize MPASOVolume
+
+        :data: (1D numpy array) data at each location
+        :lon: (1D numpy array) longitude
+        :lat: (1D numpy array) latitude
+        :depth: (1D numpy array) depth
+        :cellarea: (1D numpy array) area of cells
+        :name: (str) name of variable
+        :units: (str) units of variable
+
+        """
+        self.fillvalue = -1.e30
+        self.data = np.where(data<=self.fillvalue, np.nan, data)
+        self.lon = lon
+        self.lat = lat
+        self.depth = depth
+        self.cellarea = cellarea
+        self.name = name
+        self.units = units
+
+    def get_map(self, depth=0.0):
+        """ Return a map at depth
+
+        :depth: (float) depth of the cross section
+        :returns: (MPASOMap object) map
+
+        """
+        # get z index
+        zidx = np.argmin(np.abs(self.depth-depth))
+        # MPASOMap object
+        obj = MPASOMap(data=self.data[:,zidx], lon=self.lon, lat=self.lat, cellarea=self.cellarea, name=self.name, units=self.units)
+        return obj
+
+    def get_vertical_cross_section(self, lon0, lat0, lon1, lat1, depth_bottom=5000.0, depth_top=0.0):
+        """Return cross section defined by two points [lon0, lat0] and [lon1, lat1] and
+           the depth range [depth_bottom, depth_top]
+
+        :lon0: (float) Longitude of starting point
+        :lat0: (float) Latitude of starting point
+        :lon1: (float) Longitude of ending point
+        :lat1: (float) Latitude of ending point
+        :depth_bottom: (float) depth of the bottom
+        :depth_top: (float) depth of the top
+        :returns: (MPASOVertCrossSection object) vertical cross section
+
+        """
+        # automatically adjust number of points
+        d = distance(lon0, lat0, lon1, lat1)
+        d_cell = np.sqrt(self.cellarea.mean())
+        npoints = int(np.ceil(d/d_cell*1000))
+        print('Nearest neighbor interpolation to {} points.'.format(npoints))
+        xx = np.linspace(lon0, lon1, npoints)
+        yy = np.linspace(lat0, lat1, npoints)
+        # select nearest neighbor
+        pts = np.array(list(zip(xx,yy)))
+        tree = spatial.KDTree(list(zip(self.lon, self.lat)))
+        p = tree.query(pts)
+        cidx = p[1]
+        zidx0 = np.argmin(np.abs(self.depth-depth_top))
+        zidx1 = np.argmin(np.abs(self.depth-depth_bottom))
+        data = self.data[cidx,zidx0:zidx1]
+        depth = self.depth[zidx0:zidx1]
+        # calculate distance from [lon0, lat0]
+        dist = np.zeros(npoints)
+        for i in np.arange(npoints-1):
+            dist[i+1] = distance(lon0, lat0, xx[i+1], yy[i+1])
+        obj = MPASOVertCrossSection(data=data, lon=xx, lat=yy, dist=dist, depth=depth, name=self.name, units=self.units)
+        return obj
 
 #--------------------------------
 # MPASOMap
@@ -14,7 +93,7 @@ class MPASOMap(object):
 
     """MPASOMap object"""
 
-    def __init__(self, data=None, lon=None, lat=None, cellarea=None, name=None, units=None):
+    def __init__(self, data, lon, lat, cellarea, name, units):
         """Initialize MPASOMap
 
         :data: (1D numpy array) data at each location
@@ -77,7 +156,7 @@ class MPASOMap(object):
         :add_colorbar: (bool) do not add colorbar if False
         :cmap: (str, optional) colormap
         :**kwargs: (keyword arguments) to be passed to mpl_toolkits.basemap.scatter()
-        :return: (matplotlib figure object) figure
+        :return: (basemap) figure
 
         """
         # use curret axis if not specified
@@ -105,7 +184,8 @@ class MPASOMap(object):
             lon = np.where(lon < 20., lon+360., lon)
         else:
             # regional map
-            [lon_ll, lat_ll, lon_ur, lat_ur] = region_latlon(region)
+            region_obj = region_latlon(region)
+            lon_ll, lat_ll, lon_ur, lat_ur = region_obj.lon_ll, region_obj.lat_ll, region_obj.lon_ur, region_obj.lat_ur
             lon_c = 0.5*(lon_ll+lon_ur)
             lat_c = 0.5*(lat_ll+lat_ur)
             m = Basemap(projection='cass', llcrnrlon=lon_ll, llcrnrlat=lat_ll, urcrnrlon=lon_ur, urcrnrlat=lat_ur,
@@ -163,24 +243,24 @@ class MPASOMap(object):
             cb = m.colorbar(fig, ax=axis)
             cb.formatter.set_powerlimits((-4, 4))
             cb.update_ticks()
-        return fig
+        return m
 
 #--------------------------------
-# MPASOCrossSection
+# MPASOVertCrossSection
 #--------------------------------
 
-class MPASOCrossSection(object):
+class MPASOVertCrossSection(object):
 
-    """MPASOCrossSection object"""
+    """MPASOVertCrossSection object"""
 
-    def __init__(self, data=None, lon0=None, lat0=None, lon1=None, lat1=None name=None, units=None):
+    def __init__(self, data, lon, lat, dist, depth, name, units):
         """Initialize MPASOCrossSection
 
-        :data: (1D numpy array) data at each location
-        :lon0: (1D numpy array) longitude of starting point
-        :lat0: (1D numpy array) latitude of starting point
-        :lon1: (1D numpy array) longitude of ending point
-        :lat1: (1D numpy array) latitude of ending point
+        :data: (1D numpy array) data array
+        :lon: (1D numpy array) longitude array
+        :lat: (1D numpy array) latitude array
+        :dist: (1D numpy array) distance array
+        :depth: (1D numpy array) depth array
         :name: (str) name of variable
         :units: (str) units of variable
 
@@ -188,36 +268,96 @@ class MPASOCrossSection(object):
         self.data = data
         self.lon = lon
         self.lat = lat
+        self.dist = dist
+        self.depth = depth
         self.name = name
         self.units = units
+
+    def plot(self, axis=None, ptype='contourf', levels=None, add_title=True, add_colorbar=True, cmap='rainbow', **kwargs):
+        """Plot scatters on a map
+
+        :axis: (matplotlib.axes, optional) axis to plot figure on
+        :leveles: (list, optional) list of levels
+        :add_title: (bool) do not add title if False
+        :add_colorbar: (bool) do not add colorbar if False
+        :cmap: (str, optional) colormap
+        :**kwargs: (keyword arguments) to be passed to mpl_toolkits.basemap.scatter()
+        :return: (basemap) figure
+
+        """
+        # use curret axis if not specified
+        if axis is None:
+            axis = plt.gca()
+        if levels:
+            bounds = np.array(levels)
+            norm = colors.BoundaryNorm(boundaries=bounds, ncolors=256)
+        else:
+            norm = None
+        if ptype == 'pcolor':
+            fig = axis.pcolor(self.dist, self.depth, np.transpose(self.data), norm=norm, cmap=plt.cm.get_cmap(cmap), **kwargs)
+        elif ptype == 'contourf':
+            fig = axis.contourf(self.dist, self.depth, np.transpose(self.data), norm=norm, cmap=plt.cm.get_cmap(cmap),  **kwargs)
+
+        axis.set_xlabel('Distance (km)')
+        axis.set_ylabel('Depth (m)')
+        axis.invert_yaxis()
+        # add title
+        if add_title:
+            axis.set_title('{} ({})'.format(self.name, self.units))
+        # add colorbar
+        if add_colorbar:
+            cb = plt.colorbar(fig, ax=axis)
+            cb.formatter.set_powerlimits((-4, 4))
+            cb.update_ticks()
+        return fig
+
+#--------------------------------
+# Region object
+#--------------------------------
+
+class regionMap(object):
+    """ Region object"""
+    def __init__(self, lon_ll, lat_ll, lon_ur, lat_ur):
+        self.lon_ll = lon_ll
+        self.lat_ll = lat_ll
+        self.lon_ur = lon_ur
+        self.lat_ur = lat_ur
 
 #--------------------------------
 # Share functions
 #--------------------------------
 
-def region_latlon(region):
+def region_latlon(region_name):
     """Return longitude and latitude of lower left an upper right of the region.
 
-    :region: (string) region name
-    :return: (list) lon_ll, lat_ll, lon_ur, lat_ur
+    :region_name: (string) region name
+    :return: (region object) predefined region object
 
     """
 
-    if region == 'LabSea':
+    if region_name == 'LabSea':
         # regional map for Labrador sea
-        lon_ll = 296.0
-        lat_ll = 36.0
-        lon_ur = 356.0
-        lat_ur = 70.0
-    elif region == 'test':
-        lon_ll = 310.0
-        lat_ll = 55.0
-        lon_ur = 320.0
-        lat_ur = 65.0
+        region = regionMap(lon_ll=296.0, lat_ll=36.0, lon_ur=356.0, lat_ur=70.0)
+    elif region_name == 'test':
+        region = regionMap(lon_ll=310.0, lat_ll=55.0, lon_ur=320.0, lat_ur=65.0)
     else:
-        raise ValueError('Region {} not supported.'.format(region))
-    return [lon_ll, lat_ll, lon_ur, lat_ur]
+        raise ValueError('Region {} not supported.'.format(region_name))
+    return region
 
+def distance(lon0, lat0, lon1, lat1):
+    """Calculate the distance between two points on Earth.
+    https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude
+
+    """
+    radius = 6371  # km
+    dlat = np.radians(lat1 - lat0)
+    dlon = np.radians(lon1 - lon0)
+    a = (np.sin(dlat / 2) * np.sin(dlat / 2) +
+         np.cos(np.radians(lat0)) * np.cos(np.radians(lat1)) *
+         np.sin(dlon / 2) * np.sin(dlon / 2))
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+    d = radius * c
+    return d
 
 def plot_map(xx, yy, data, axis=None, levels=None, add_colorbar=True, draw_eq=False, cmap='rainbow', **kwargs):
     # use curret axis if not specified
