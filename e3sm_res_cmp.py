f7021fb7ad90bd8cc095df846f067c594c4c3fea
Qing Li <lqingpku@gmail.com>
2019-05-23 12:51:51 -0500

    Update paths

--- e3sm_res_cmp.py
+++ e3sm_res_cmp.py
@@ -4,7 +4,7 @@ import os
 # Functions for E3SM resolution comparsion
 #--------------------------------
 
-def load_paths_ocn(climo_ys=41, climo_ye=50, ts_ys=1, ts_ye=50):
+def load_paths_ocn(climo_ys=41, climo_ye=50, ts_ys=1, ts_ye=50, runname=None):
     """Load paths of ocean data
     :climo_ys: starting year of climatology
     :climo_ye: ending year of climatology
@@ -36,24 +36,29 @@ def load_paths_ocn(climo_ys=41, climo_ye=50, ts_ys=1, ts_ye=50):
         mon_root = data_root+'/archive/ocn/hist'
     elif 'blues' in hostname:
         yshift = 0
-        # data_root = '/lcrc/group/acme/jwolfe/acme_scratch/anvil/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil'
-        # climo_root = '/lcrc/group/acme/qingli/e3sm_climo/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil/{:04d}-{:04d}/ocn'.format(climo_ys+yshift, climo_ye+yshift)
-        # ts_root = '/lcrc/group/acme/qingli/e3sm_ts/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil/{:04d}-{:04d}/ocn'.format(ts_ys+yshift, ts_ye+yshift)
-        # fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/low_res-noSI/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
-        # rst_root = data_root+'/run'
-        # mon_root = data_root+'/run'
-        # data_root = '/lcrc/group/acme/qingli/acme_scratch/anvil/GMPAS-IAF_T62_oEC60to30v3_CTRL'
-        # climo_root = '/lcrc/group/acme/qingli/e3sm_climo/GMPAS-IAF_T62_oEC60to30v3_CTRL/{:04d}-{:04d}/ocn'.format(climo_ys+yshift, climo_ye+yshift)
-        # ts_root = '/lcrc/group/acme/qingli/e3sm_ts/GMPAS-IAF_T62_oEC60to30v3_CTRL/{:04d}-{:04d}/ocn'.format(ts_ys+yshift, ts_ye+yshift)
-        # fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/low_res-g/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
-        # rst_root = data_root+'/run'
-        # mon_root = data_root+'/run'
-        data_root = '/lcrc/group/acme/jwolfe/acme_scratch/anvil/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil'
-        climo_root = '/lcrc/group/acme/qingli/e3sm_climo/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil/{:04d}-{:04d}/ocn'.format(climo_ys+yshift, climo_ye+yshift)
-        ts_root = '/lcrc/group/acme/qingli/e3sm_ts/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil/{:04d}-{:04d}/ocn'.format(ts_ys+yshift, ts_ye+yshift)
-        fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/gl-mesh/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
-        rst_root = data_root+'/run'
-        mon_root = data_root+'/run'
+        if runname == 'low-res-noSI':
+            data_root = '/lcrc/group/acme/jwolfe/acme_scratch/anvil/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil'
+            climo_root = '/lcrc/group/acme/qingli/e3sm_climo/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil/{:04d}-{:04d}/ocn'.format(climo_ys+yshift, climo_ye+yshift)
+            ts_root = '/lcrc/group/acme/qingli/e3sm_ts/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil/{:04d}-{:04d}/ocn'.format(ts_ys+yshift, ts_ye+yshift)
+            fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/low_res-noSI/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
+            rst_root = data_root+'/run'
+            mon_root = data_root+'/run'
+        elif runname == 'low-res-g':
+            data_root = '/lcrc/group/acme/qingli/acme_scratch/anvil/GMPAS-IAF_T62_oEC60to30v3_CTRL'
+            climo_root = '/lcrc/group/acme/qingli/e3sm_climo/GMPAS-IAF_T62_oEC60to30v3_CTRL/{:04d}-{:04d}/ocn'.format(climo_ys+yshift, climo_ye+yshift)
+            ts_root = '/lcrc/group/acme/qingli/e3sm_ts/GMPAS-IAF_T62_oEC60to30v3_CTRL/{:04d}-{:04d}/ocn'.format(ts_ys+yshift, ts_ye+yshift)
+            fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/low_res-g/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
+            rst_root = data_root+'/run'
+            mon_root = data_root+'/run'
+        elif runname == 'gl-mesh':
+            data_root = '/lcrc/group/acme/jwolfe/acme_scratch/anvil/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil'
+            climo_root = '/lcrc/group/acme/qingli/e3sm_climo/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil/{:04d}-{:04d}/ocn'.format(climo_ys+yshift, climo_ye+yshift)
+            ts_root = '/lcrc/group/acme/qingli/e3sm_ts/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil/{:04d}-{:04d}/ocn'.format(ts_ys+yshift, ts_ye+yshift)
+            fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/gl-mesh/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
+            rst_root = data_root+'/run'
+            mon_root = data_root+'/run'
+        else:
+            raise ValueError('Run \'{}\' not supported'.format(runname))
     elif 'pn1803144' in hostname:
         # for testing
         data_root = os.environ['HOME']+'/data/mpas/test'
@@ -72,7 +77,7 @@ def load_paths_ocn(climo_ys=41, climo_ye=50, ts_ys=1, ts_ye=50):
             'fig_root': fig_root}
     return path
 
-def load_paths_ice(climo_ys=41, climo_ye=50, ts_ys=1, ts_ye=50):
+def load_paths_ice(climo_ys=41, climo_ye=50, ts_ys=1, ts_ye=50, runname=None):
     """Load paths of sea ice data
     :climo_ys: starting year of climatology
     :climo_ye: ending year of climatology
@@ -104,24 +109,29 @@ def load_paths_ice(climo_ys=41, climo_ye=50, ts_ys=1, ts_ye=50):
         mon_root = data_root+'/archive/ice/hist'
     elif 'blues' in hostname:
         yshift = 0
-        # data_root = '/lcrc/group/acme/jwolfe/acme_scratch/anvil/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil'
-        # climo_root = '/lcrc/group/acme/qingli/e3sm_climo/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil/{:04d}-{:04d}/ice'.format(climo_ys+yshift, climo_ye+yshift)
-        # ts_root = '/lcrc/group/acme/qingli/e3sm_ts/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil/{:04d}-{:04d}/ice'.format(ts_ys+yshift, ts_ye+yshift)
-        # fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/low_res-noSI/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
-        # rst_root = data_root+'/run'
-        # mon_root = data_root+'/run'
-        # data_root = '/lcrc/group/acme/qingli/acme_scratch/anvil/GMPAS-IAF_T62_oEC60to30v3_CTRL'
-        # climo_root = '/lcrc/group/acme/qingli/e3sm_climo/GMPAS-IAF_T62_oEC60to30v3_CTRL/{:04d}-{:04d}/ice'.format(climo_ys+yshift, climo_ye+yshift)
-        # ts_root = '/lcrc/group/acme/qingli/e3sm_ts/GMPAS-IAF_T62_oEC60to30v3_CTRL/{:04d}-{:04d}/ice'.format(ts_ys+yshift, ts_ye+yshift)
-        # fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/low_res-g/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
-        # rst_root = data_root+'/run'
-        # mon_root = data_root+'/run'
-        data_root = '/lcrc/group/acme/jwolfe/acme_scratch/anvil/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil'
-        climo_root = '/lcrc/group/acme/qingli/e3sm_climo/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil/{:04d}-{:04d}/ice'.format(climo_ys+yshift, climo_ye+yshift)
-        ts_root = '/lcrc/group/acme/qingli/e3sm_ts/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil/{:04d}-{:04d}/ice'.format(ts_ys+yshift, ts_ye+yshift)
-        fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/gl-mesh/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
-        rst_root = data_root+'/run'
-        mon_root = data_root+'/run'
+        if runname == 'low-res-noSI':
+            data_root = '/lcrc/group/acme/jwolfe/acme_scratch/anvil/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil'
+            climo_root = '/lcrc/group/acme/qingli/e3sm_climo/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil/{:04d}-{:04d}/ice'.format(climo_ys+yshift, climo_ye+yshift)
+            ts_root = '/lcrc/group/acme/qingli/e3sm_ts/20190212.A_WCYCL1950S_CMIP6_LRtunedHR-noCNT.ne30_oECv3_ICG.anvil/{:04d}-{:04d}/ice'.format(ts_ys+yshift, ts_ye+yshift)
+            fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/low_res-noSI/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
+            rst_root = data_root+'/run'
+            mon_root = data_root+'/run'
+        elif runname == 'low-res-g':
+            data_root = '/lcrc/group/acme/qingli/acme_scratch/anvil/GMPAS-IAF_T62_oEC60to30v3_CTRL'
+            climo_root = '/lcrc/group/acme/qingli/e3sm_climo/GMPAS-IAF_T62_oEC60to30v3_CTRL/{:04d}-{:04d}/ice'.format(climo_ys+yshift, climo_ye+yshift)
+            ts_root = '/lcrc/group/acme/qingli/e3sm_ts/GMPAS-IAF_T62_oEC60to30v3_CTRL/{:04d}-{:04d}/ice'.format(ts_ys+yshift, ts_ye+yshift)
+            fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/low_res-g/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
+            rst_root = data_root+'/run'
+            mon_root = data_root+'/run'
+        elif runname == 'gl-mesh':
+            data_root = '/lcrc/group/acme/jwolfe/acme_scratch/anvil/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil'
+            climo_root = '/lcrc/group/acme/qingli/e3sm_climo/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil/{:04d}-{:04d}/ice'.format(climo_ys+yshift, climo_ye+yshift)
+            ts_root = '/lcrc/group/acme/qingli/e3sm_ts/20190419.test.A_WCYCL1850.ne30_oGNLD30to10.anvil/{:04d}-{:04d}/ice'.format(ts_ys+yshift, ts_ye+yshift)
+            fig_root = os.environ['HOME']+'/work/e3sm_res_cmp/figures/gl-mesh/{:04d}-{:04d}'.format(climo_ys+yshift, climo_ye+yshift)
+            rst_root = data_root+'/run'
+            mon_root = data_root+'/run'
+        else:
+            raise ValueError('Run \'{}\' not supported'.format(runname))
     elif 'pn1803144' in hostname:
         # for testing
         data_root = os.environ['HOME']+'/data/mpas/test'
